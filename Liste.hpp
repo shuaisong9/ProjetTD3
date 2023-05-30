4f75c3e4bdc9d2aaf9ba0fb6415d24c96911d29c
Ikramarroud <112962249+Ikramarroud@users.noreply.github.com>
2023-05-30 19:46:51 -0400

    Update Liste.hpp

--- Liste.hpp
+++ Liste.hpp
@@ -4,83 +4,88 @@
 #include <cassert>
 #include "gsl/span"
 #include "cppitertools/range.hpp"
-using namespace std;
-
+//version 2
 
 template <typename T>
 class Liste
 {
 public:
 	//TODO: Constructeurs et surcharges d'op�rateurs
-	Liste() {
-		nElements_ = 0;
-		capacite_ = 1;
-		elements_ = make_unique<shared_ptr<T>[]>(capacite_);
+    Liste() = default;
+
+	Liste(const Liste<T>& liste) {
+		nElements_ = liste.size();
+		capacite_ = liste.getCapacite();
+		elements_ = liste.getElement;
 	}
 
-	shared_ptr<T> operator[] (unsigned index) const {	// const ou non??
-		return elements_[index];
-	}
-	shared_ptr<T> operator[] (unsigned index) {	// const ou non??
-		return elements_[index];
-	}
-		// Operateur de copie pour regler probleme de build: 
-		// Attempting to reference a deleted function 
+	shared_ptr<T[]> getElement() const { return elements_; }
+	int getCapacite() { return capacite_ };
+	int getNbElements() { return nElements_; }
 
-	//
+
+	//ostream& operator<<(ostream& out, )
+
+	shared_ptr<T> operator[](int index) const { return elements_[index]; }
+
+
 	//TODO: M�thode pour ajouter un �l�ment � la liste
-	void ajouterElement(const std::shared_ptr<T>& element) {
-		elements.push_back(element);
-		nElements_++;
+	void ajouterElement(unique_ptr<shared_ptr<T>[]> element) {
+		if (nElements == capacite)
+			changerCapacite(max(1, capacite * 2));
+		elements[nElements++] = move(element);
+	}
+	
+	friend ostream& operator <<(ostream& out, const Liste& eleList) {
+		for (auto& i : range(eleList.size())) {
+			out << eleList.elements[i]<<endl;
+		}
 	}
 
-	// Pour size, on utilise le m�me nom que les accesseurs de la biblioth�que standard, qui permet d'utiliser certaines fonctions de la bibliotheque sur cette classe.
+	// Pour size, on utilise le m�me nom que les accesseurs de la biblioth�que standard,
+	//qui permet d'utiliser certaines fonctions de la bibliotheque sur cette classe.
 	unsigned size() const         { return nElements_; }
 	unsigned getCapacite() const  { return capacite_; }
 
-	//TODO: M�thode pour changer la capacit� de la liste
 
-    void changerCapacite(int nouvelleCapacite){
-        auto nouvelle_list = make_unique<T[]>(nouvelleCapacite);
-        if(elements_!= nullptr){
-            nElements_ = min(nouvelleCapacite, nElements_){
-                for(int i=0; i<nElements_; i++){
-                    nouvelle_list[i] = move(elements_[i]);
-                }
-            }
-        }
-        elements_ =move(nouvelleCapacite);
-        capacite_ = nouvelleCapacite;
-    }
+	//TODO: M�thode pour changer la capacit� de la liste
+	void changerCapacite(unsigned nvcapacite) {
+		auto nouvelle_liste = make_unique<shared_ptr<T>[]>(nvcapacite);
+		if (elements != nullptr) {
+			nElements = min(nvcapacite, nElements_) { //this is working
+				for (int i = 0; i < nElements_; i++) {
+					nouvelle_liste = move(elements_[i]);
+				}
+			}
+		}
+		elements_ = move(nvcapacite);
+		capacite_ = nvcapacite;
+	}
+
+	//shared_ptr<T> operator[] (unsigned int index) const { return elements_[index];}
+
+	shared_ptr <T> trouverElement(const Liste<T>& liste, const function<bool(T)>& critere) {
+		shared_ptr<T> ptr = nullptr;
+		for (int i = 0; i < liste.size(); i++) {
+			if (critere(liste.getElement()[i])) {
+				ptr = liste.getElement()[i];
+			}
+		}
+		return ptr;
+	}
+
 
 	//TODO: M�thode pour trouver une �l�ment selon un crit�re (lambda).
-	template <typename Conteneur, typename PredicatUnaire>
-	auto trouverElement(const Conteneur& valeurs, const PredicatUnaire& critere)
-	{
-		auto iter = std::ranges::find_if(valeurs, critere);
-		return (iter != valeurs.end()) ? *it : nullptr;
-		// Return: reference � smartPtr
-	}
-
-	template <typename PredicatUnaire>
-	auto trouverElement2(const PredicatUnaire& critere)
-	{
-		auto iter = std::ranges::find_if(elements_, critere);
-		return (iter != elements_.end()) ? *it : nullptr;
-		// Return: reference � smartPtr
-	}
-
+	
+	size_t nElements = 0;
+	size_t capacite = 0;
 
 private:
 	unsigned nElements_;
 	unsigned capacite_;
 	//TODO: Attribut contenant les �l�ments de la liste.
-	unique_ptr<shared_ptr<T>[]> elements_;
-
-	//vector<unique_ptr<Item>> items;
-	//auto unItem = make_unique<Item>();
-	//items.push_back(move(unItem));
+    //unique_ptr<shared_ptr<T>[]> elements_; 
+	shared_ptr<T [] > elements_;
 };
-
 
 
