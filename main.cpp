06d0d59ea50a65ec083986cbb1bbd5e573f5acf5
Ikramarroud <112962249+Ikramarroud@users.noreply.github.com>
2023-05-30 19:48:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include "bibliotheque_cours.hpp"
+#include "bibliotheque_cours.hpp"
 #include "verification_allocation.hpp"
 #include "Liste.hpp"
 #include "Concepteur.hpp"
@@ -10,6 +10,28 @@ using namespace std;
 
 //TODO: Vos surcharges d'opérateur <<
 
+ostream& operator<<(ostream& out, const Concepteur& c) {
+	out << "Nom " << c.getNom() << "\t" << " Annee Naissance " << c.getAnneeNaissance() << "\t" << " Pays " << c.getPays() << "\t" << endl;
+	return out;
+}
+/*
+ostream& operator <<(ostream& out, Jeu& j) {
+	out << "Titre " << j.getTitre() << "\t" << j.getAnneeSortie() << "\t" << j.getDeveloppeur() << "\n";
+	for (int i = 0; j.getConcepteurs().size()) > i; i++ {
+
+	}
+
+}
+
+
+ostream& operator<<(ostream& out, const Liste<Jeu>& listeJeux) {
+	for (int i = 0; i < listeJeux.size(); i++) {
+		out<<listeJeux.get
+	}
+}
+*/
+
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
 	#pragma region "Bibliothèque du cours"
@@ -18,22 +40,26 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 	// les supportent normalement par défaut.
 	bibliotheque_cours::activerCouleursAnsi(); 
 	#pragma endregion
+
+
+
 	
 	Liste<Jeu> lj = creerListeJeux("jeux.bin");
 	static const string ligneSeparation = "\n\033[92m"
 		"══════════════════════════════════════════════════════════════════════════"
 		"\033[0m\n";
-	
-
-	cout << lj[2].getTitre() << endl;
-	//cout << lj[1].det
-
 
 	//TODO: Les l'affichage et l'écriture dans le fichier devraient fonctionner.
 	//cout << ligneSeparation << lj;
 	//ofstream("sortie.txt") << lj;
 
-	//TODO: Compléter le main avec les tests demandés.
+
+
+
+	//TODO: Compléter le main avec les tests demandés.\
+	// Operator
+	cout << "Titre a indice 2: " << lj[2]->titre;
+
 	//TODO: S'assurer qu'aucune ligne de code est non couverte.
 	//NOTE: Il n'est pas nécessaire de couvrir les getters/setters simples fournis; il faut tester si vous en ajoutez ou les modifiez.
 	//NOTE: Pour Liste, qui est générique, on demande de couvrir uniquement pour Liste<Jeu>, pas pour tous les types.
